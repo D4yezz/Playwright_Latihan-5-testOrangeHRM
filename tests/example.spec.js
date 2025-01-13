546d4d7768c418f9c326a7c30ad039f6a00acf3d
Dayezzz <adiasmuhsin1206@gmail.com>
2025-01-13 16:04:03 +0700

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Login', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.fill('input[name="username"]', "Admin");
+  await page.fill('input[name="password"]', "admin123");
+  await page.click('button[type="submit"]');
+  await page.getByRole('link', { name: 'PIM' }).click();
+  await page.fill('div.oxd-autocomplete-text-input>input', "Anastacio");
+  await page.click('button[type="submit"]');
+});
+
+
